2f18df1507f7051cf220919f956e11f7aead7750
NematSiddique <nemat.s3301@gmail.com>
2025-05-28 18:56:57 +0530

    updates

--- components/intro.tsx
+++ components/intro.tsx
@@ -83,7 +83,7 @@ export default function Intro() {
           
         <a 
           className='bg-white p-4 text-gray-700 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60 dark:hover:text-white/90' 
-          href='https://www.linkedin.com/in/nematsiddique/' 
+          href='https://www.linkedin.com/in/nematsiddique' 
           target='_blank' 
           rel='noopener noreferrer' 
           title='LinkedIn Profile'
